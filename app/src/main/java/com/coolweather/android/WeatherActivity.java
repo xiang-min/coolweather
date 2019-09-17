225a13e71a45f1f5b94235d0939f70fe3cdf2b68
xiangmincode <980003162@qq.com>
2019-09-17 15:33:03 +0800

    新增后台自动更新天气的功能

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -208,6 +210,8 @@ public class WeatherActivity extends AppCompatActivity {
         if(weather.aqi != null){
             aqiText.setText(weather.aqi.city.aqi);
             pm25Text.setText(weather.aqi.city.pm25);
+            Intent intent = new Intent(this, AutoUpdateService.class);
+            startService(intent);
         }
 
         String comfort = "舒适度：" + weather.suggestion.comfort.info;
